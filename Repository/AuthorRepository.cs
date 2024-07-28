ff6e53b15407b3e765232eb7321fe691cd6241de
Катя <katya.dendisol@gmail.com>
2024-07-28 15:02:09 +0300

    Add repositories for each entities

---
+++ Repository/AuthorRepository.cs
@@ -0,0 +1,17 @@
+﻿using Contracts;
+using Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+  public class AuthorRepository: RepositoryBase<Author>, IAuthorRepository
+  {
+    public AuthorRepository(RepositoryContext repositoryContext) : base(repositoryContext)
+    {
+    }
+  }
+}
